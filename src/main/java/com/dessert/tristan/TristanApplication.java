710441d399bda444d69245ee17ec4a023be291e0
= <846587073@qq.com>
2022-02-28 21:08:33 -0500

    create project

---
+++ src/main/java/com/dessert/tristan/TristanApplication.java
@@ -0,0 +1,13 @@
+package com.dessert.tristan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TristanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TristanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dessert/tristan/TristanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dessert.tristan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TristanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
